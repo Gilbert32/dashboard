93750526e1e6b2bd0f8837ee6531289589db90a3
IsmailShehab1 <ismailshehab1627@gmail.com>
2019-10-11 13:59:18 +0300

    no message

--- src/app/widget3/widget3.component.ts
+++ src/app/widget3/widget3.component.ts
@@ -1,13 +1,9 @@
-<<<<<<< HEAD
+
 import {Location} from '../Models/Location';
 import { Component, OnInit } from '@angular/core';
 import {Widget3SService} from '../Services/widget3-s.service';
 import {GridSService} from '../Services/grid-s.service';
-=======
-import {Location} from '../Location';
-import { Component, OnInit } from '@angular/core';
-import {Widget3SService} from '../widget3-s.service';
->>>>>>> 25ea44673f1f1cee4243bd7c127d44136a1b48c7
+
 
 @Component({
   selector: 'app-widget3',
